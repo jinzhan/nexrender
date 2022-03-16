97736c745726bbc98a1dd72419115d8fc4a89f37
Jinzhan <steinitz@qq.com>
2022-03-16 15:24:47 +0800

    chore: rm unused pkg

--- packages/nexrender-core/src/index.js
+++ packages/nexrender-core/src/index.js
@@ -28,7 +28,6 @@ if (process.env.NEXRENDER_REQUIRE_PLUGINS) {
     require('@nexrender/action-encode');
     require('@nexrender/action-upload');
     require('@nexrender/action-cache');
-
     require('@nexrender/provider-s3');
     require('@nexrender/provider-ftp');
     require('@nexrender/provider-gs');
